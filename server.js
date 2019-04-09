48655e8910e3935cd7fec2787b017490c17538ae
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-04-09 16:33:36 -0700

    Implement functional proxy server with two modules

--- server.js
+++ server.js
@@ -1,16 +1,67 @@
 const express = require('express');
 const path = require('path');
-
+const morgan = require('morgan');
+const axios = require('axios');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+app.use(morgan('dev'));
 app.use('/', express.static(__dirname + '/public/'));
 app.use('/stocks/:stockID', express.static(__dirname + '/public/'));
 
-app.get('/', (req, res) => {
-  res.send();
+const axios3001 = axios.create({
+  baseURL: 'localhost:3001', 
+  timeout: 3000,
+})
+const axios3002 = axios.create({
+  baseURL: 'localhost:3002', 
+  timeout: 3000,
+})
+const axios3003 = axios.create({
+  baseURL: 'localhost:3003', 
+  timeout: 3000,
+})
+const axios3004 = axios.create({
+  baseURL: 'localhost:3004', 
+  timeout: 3000,
+})
+
+app.use('/api/ratings/:stockID', (req, res) => {
+  axios3001.get(`/api/ratings/${req.params.stockID}`)
+    .then(result => res.send(result.data))
+    .catch(err => res.send(err));
 });
 
+app.use('/api/history/:stockID', (req, res) => {
+  axios3001.get(`/api/history/${req.params.stockID}`)
+    .then(result => res.send(result.data))
+    .catch(err => res.send(err));
+});
+
+app.use('/api/stocks/:ticker', (req, res) => {
+  axios3002.get(`/api/stocks/${req.params.ticker}`)
+    .then(response => res.send(response.data))
+    .catch(err => res.send(err));
+})
+
+app.use('/api/accounts/:account_number', (req, res) => {
+  axios3002.get(`/api/accounts/${req.params.account_number}`)
+    .then(response => res.send(response.data))
+    .catch(err => res.send(err));
+})
+
+app.use('/api/quotes/:symbol', (req, res) => {
+  axios3003.get(`/api/quotes/${req.params.symbol}`)
+    .then(response => res.send(response.data))
+    .catch(err => res.send(err));
+})
+
+app.use('/stocks/tags/:tag', (req, res) => {
+  axios3003.get(`/stocks/tags/${req.params.tag}`)
+    .then(response => res.send(response.data))
+    .catch(err => res.send(err));
+})
+
 app.listen(PORT, () => {
-  console.log(`Server now listening at ${PORT}`);
+  console.log(`Server now listening at: http://localhost:${PORT}`);
 });
--- src/index.js
+++ src/index.js
@@ -5,33 +5,33 @@
 
 
 
-import axios from 'axios';
+// import axios from 'axios';
 
-const stockID = window.location.pathname.split('/')[2];
+// const stockID = window.location.pathname.split('/')[2];
 
-axios.get(`localhost:3001/api/ratings/${stockID}`)
-  .then(res => res.data)
-  .then(result => {
-    res.send(result);
-  });
+// axios.get(`localhost:3001/api/ratings/${stockID}`)
+//   .then(res => res.data)
+//   .then(result => {
+//     res.send(result);
+//   });
  
-axios.get(`localhost:3001/api/history/${stockID}`)
-  .then(res => res.data)
-  .then(result => {
-  res.send(result);
-});
+// axios.get(`localhost:3001/api/history/${stockID}`)
+//   .then(res => res.data)
+//   .then(result => {
+//   res.send(result);
+// });
 
-axios.get(`localhost:3002/api/stock/${stockID}`)
-  .then(res => res.data)
-  .then(result => {
-  res.send(result);
-});
+// axios.get(`localhost:3002/api/stock/${stockID}`)
+//   .then(res => res.data)
+//   .then(result => {
+//   res.send(result);
+// });
 
-axios.get(`localhost:3002/api/accounts/${stockID}`)
-  .then(res => res.data)
-  .then(result => {
-  res.send(result);
-});
+// axios.get(`localhost:3002/api/accounts/${stockID}`)
+//   .then(res => res.data)
+//   .then(result => {
+//   res.send(result);
+// });
 
 //  TODO: charts
 //  TODO: aboutinfo
