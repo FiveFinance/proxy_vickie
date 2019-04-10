551922391ff4c2f155035183ce9abb159a5892a3
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-04-09 18:00:00 -0700

    Implement working proxy server with three modules

--- server.js
+++ server.js
@@ -1,30 +1,27 @@
 const express = require('express');
 const path = require('path');
-const morgan = require('morgan');
 const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
-app.use('/', express.static(__dirname + '/public/'));
-app.use('/stocks/:stockID', express.static(__dirname + '/public/'));
+app.use('/', express.static(path.join(__dirname, '/public/')));
+app.use('/stocks/:stockID', express.static(path.join(__dirname, '/public/')));
 
 const axios3001 = axios.create({
-  baseURL: 'localhost:3001', 
-  timeout: 3000,
-})
+  baseURL: 'http://localhost:3001',
+});
+
 const axios3002 = axios.create({
-  baseURL: 'localhost:3002', 
-  timeout: 3000,
-})
+  baseURL: 'http://localhost:3002',
+});
+
 const axios3003 = axios.create({
-  baseURL: 'localhost:3003', 
-  timeout: 3000,
-})
+  baseURL: 'http://localhost:3003',
+});
+
 const axios3004 = axios.create({
-  baseURL: 'localhost:3004', 
-  timeout: 3000,
-})
+  baseURL: 'http://localhost:3004',
+});
 
 app.use('/api/ratings/:stockID', (req, res) => {
   axios3001.get(`/api/ratings/${req.params.stockID}`)
@@ -52,13 +49,13 @@ app.use('/api/accounts/:account_number', (req, res) => {
 
 app.use('/api/quotes/:symbol', (req, res) => {
   axios3003.get(`/api/quotes/${req.params.symbol}`)
-    .then(response => res.send(response.data))
+    .then(result => res.send(result.data))
     .catch(err => res.send(err));
 })
 
 app.use('/stocks/tags/:tag', (req, res) => {
   axios3003.get(`/stocks/tags/${req.params.tag}`)
-    .then(response => res.send(response.data))
+    .then(result => res.send(result.data))
     .catch(err => res.send(err));
 })
 
